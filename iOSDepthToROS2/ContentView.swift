2b0ddf1892e9fdaf9ec5f103853373af83f90cfa
ion206 <ayansyedca@gmail.com>
2025-12-10 23:45:52 -0800

    Initial Commit

---
+++ iOSDepthToROS2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSDepthToROS2
+//
+//  Created by Ayan Syed on 12/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSDepthToROS2/iOSDepthToROS2App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSDepthToROS2App.swift
+//  iOSDepthToROS2
+//
+//  Created by Ayan Syed on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSDepthToROS2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
